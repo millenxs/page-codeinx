153180c31f00a173a3d3e89dba8622f12f876fa3
millenxs <milenameedeiros@gmail.com>
2024-03-05 20:50:16 -0300

    Alterações feitas

--- script.js
+++ script.js
@@ -19,3 +19,76 @@ document.getElementById('emailButton').addEventListener('click', function(){
 document.getElementById('whatsappButton').addEventListener('click', function(){
 	window.location.href = 'https://wa.link/ytownh';
 });
+
+//typewriter
+const textEl = document.querySelector(".text");
+
+const words = ["Parceira Digital", "Inovadora", "Criativa", "Transparente", "Apaixonada"].map((word) => word + ".");
+
+let wordIndex = 0;
+let charIndex = 0;
+
+let addingChars = true;
+let shouldWait = false;
+
+let currentWord = words[wordIndex];
+
+const updateCurrWord = () => {
+	wordIndex++;
+
+	if (wordIndex === words.length) wordIndex = 0;
+
+	currentWord = words[wordIndex];
+};
+
+const addChar = () => {
+	let currChar = currentWord[charIndex];
+
+	const char = document.createElement("span");
+
+	char.innerText = currChar;
+	char.classList.add("char");
+
+	textEl.appendChild(char);
+
+	charIndex++;
+
+	if (charIndex === currentWord.length) {
+		charIndex--;
+		addingChars = false;
+		shouldWait = true;
+	}
+};
+
+const removeChar = () => {
+	const char = textEl.lastElementChild;
+
+	textEl.removeChild(char);
+
+	charIndex--;
+
+	if (charIndex < 0) {
+		charIndex++;
+		addingChars = true;
+		updateCurrWord();
+	}
+};
+
+const runTypewriter = () => {
+	const operation = addingChars ? addChar : removeChar;
+
+	operation();
+
+	let timeout = addingChars ? 200 : 100;
+
+	if (shouldWait) {
+		timeout = 1000;
+		shouldWait = false;
+	}
+
+	setTimeout(runTypewriter, timeout);
+};
+
+setTimeout(runTypewriter, 1500);
+
+
