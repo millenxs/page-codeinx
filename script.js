acf30d39f969efd88034d59cf763e0d13d591b31
millenxs <milenameedeiros@gmail.com>
2024-03-07 00:08:44 -0300

    Algumas alterações de responsividade feitas

--- script.js
+++ script.js
@@ -1,93 +1,96 @@
 /* 			ROLAGEM			 */
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 	anchor.addEventListener('click', function (e) {
 	  e.preventDefault();
-
-	  document.querySelector(this.getAttribute('href')).scrollIntoView({
-		behavior: 'smooth'
-	  });
+  
+	  const target = document.querySelector(this.getAttribute('href'));
+	  if (target) {
+		const offset = target.offsetTop - 70; // Ajuste para distância da navbar, por exemplo, 70px
+		window.scrollTo({
+		  top: offset,
+		  behavior: 'smooth'
+		});
+	  }
 	});
   });
 
+
 // CONTATO
+  document.getElementById('emailButton').addEventListener('click', function() {
+    window.location.href = 'mailto:codeinx.work@gmail.com';
+  });
 
-document.getElementById('emailButton').addEventListener('click', function(){
-	window.location.href = 'mailto:codeinx.work@gmail.com';
-});
+  document.getElementById('whatsappButton').addEventListener('click', function() {
+    window.location.href = 'https://wa.link/ytownh';
+  });
 
-document.getElementById('whatsappButton').addEventListener('click', function(){
-	window.location.href = 'https://wa.link/ytownh';
-});
+  // typewriter
+  const textEl = document.querySelector(".text");
+  const wordsEl = document.querySelector(".words");
 
-//typewriter
-// typewriter
-const textEl = document.querySelector(".text");
-const wordsEl = document.querySelector(".words");
+  const words = ["Parceira", "Inovadora", "Criativa", "Transparente", "Apaixonada"].map((word) => word + ".");
 
-const words = ["Parceira Digital", "Inovadora", "Criativa", "Transparente", "Apaixonada"].map((word) => word + ".");
+  let wordIndex = 0;
+  let charIndex = 0;
 
-let wordIndex = 0;
-let charIndex = 0;
+  let addingChars = true;
+  let shouldWait = false;
 
-let addingChars = true;
-let shouldWait = false;
+  let currentWord = words[wordIndex];
 
-let currentWord = words[wordIndex];
+  const updateCurrWord = () => {
+    wordIndex++;
 
-const updateCurrWord = () => {
-  wordIndex++;
+    if (wordIndex === words.length) wordIndex = 0;
 
-  if (wordIndex === words.length) wordIndex = 0;
+    currentWord = words[wordIndex];
+  };
 
-  currentWord = words[wordIndex];
-};
+  const addChar = () => {
+    let currChar = currentWord[charIndex];
 
-const addChar = () => {
-  let currChar = currentWord[charIndex];
+    const char = document.createElement("span");
+    char.innerText = currChar;
+    char.classList.add("char");
 
-  const char = document.createElement("span");
-  char.innerText = currChar;
-  char.classList.add("char");
+    wordsEl.appendChild(char);
 
-  wordsEl.appendChild(char);
-
-  charIndex++;
-
-  if (charIndex === currentWord.length) {
-    charIndex--;
-    addingChars = false;
-    shouldWait = true;
-  }
-};
-
-const removeChar = () => {
-  const char = wordsEl.lastElementChild;
-
-  wordsEl.removeChild(char);
-
-  charIndex--;
-
-  if (charIndex < 0) {
     charIndex++;
-    addingChars = true;
-    updateCurrWord();
-  }
-};
 
-const runTypewriter = () => {
-  const operation = addingChars ? addChar : removeChar;
+    if (charIndex === currentWord.length) {
+      charIndex--;
+      addingChars = false;
+      shouldWait = true;
+    }
+  };
 
-  operation();
+  const removeChar = () => {
+    const char = wordsEl.lastElementChild;
 
-  let timeout = addingChars ? 200 : 100;
+    wordsEl.removeChild(char);
 
-  if (shouldWait) {
-    timeout = 1000;
-    shouldWait = false;
-  }
+    charIndex--;
 
-  setTimeout(runTypewriter, timeout);
-};
+    if (charIndex < 0) {
+      charIndex++;
+      addingChars = true;
+      updateCurrWord();
+    }
+  };
 
-setTimeout(runTypewriter, 1500);
+  const runTypewriter = () => {
+    const operation = addingChars ? addChar : removeChar;
+
+    operation();
+
+    let timeout = addingChars ? 200 : 100;
+
+    if (shouldWait) {
+      timeout = 1000;
+      shouldWait = false;
+    }
+
+    setTimeout(runTypewriter, timeout);
+  };
+
+  setTimeout(runTypewriter, 1500);
